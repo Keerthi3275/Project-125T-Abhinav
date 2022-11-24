d3750fd137844ce0e47d839678a8d8c8453adac9
Keerthi3275 <70685704+Keerthi3275@users.noreply.github.com>
2022-11-24 16:27:40 +0530

    Add files via upload

---
+++ classifier.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from PIL import Image
+import PIL.ImageOps
+import os, ssl, time
+
+#Code to fetch data and split the data to train and test the model(Refer hint1)
+
+
+
+X_train_scaled = X_train/255.0
+X_test_scaled = X_test/255.0
+
+clf = LogisticRegression(solver='saga', multi_class='multinomial').fit(X_train_scaled, y_train)
+
+#Code for the get_prediction function(refer hint2)
+
